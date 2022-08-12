ca713849e9d29d6af3082710b36c952c78518c4a
Stealthchair <31829676+Stealthchair@users.noreply.github.com>
2022-08-11 19:36:46 -0500

    Create a div per astronaught

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
             console.log(json);
+            const addAstronauts = document.getElementById("container");
+            let newAstronaut;
+            for(let i = 0; i < response.length; i++){
+                newAstronaut = document.createElement("div");
+                newAstronaut.setAttribute("class", "astronaut");
+                newAstronaut.innerHTML = response[i].firstname;
+            }
         });
     });
 });
